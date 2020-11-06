46c562961da6f72bfc182d4efc4248ba6e92c8af
dimcon <daimonsewell@gmail.com>
2020-11-06 17:01:51 +0200

    added vue app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="bg-dark text-light">
+
+	<div class="container bg-dark text-light" style="height:100vh">
+		<div class="w-100 h-100 d-flex justify-content-center align-items-center">
+			<img class="m-5" alt="Vue logo" src="./assets/logo.png">
+			<div class="bg-dark text-left">
+				<h1>Hello, world!</h1>
+				<h2 class="text-secondary">Congratulations. Vue is running on CodeCaps!</h2>
+			</div>
+		</div>
+	</div>
+
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
